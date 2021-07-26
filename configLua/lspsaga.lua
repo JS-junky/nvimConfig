158f2e8319f5ef1192e8503d62b69a1f4171343f
hooman bokhar <hooman@CLI-1-20.avakatan.ir>
2021-07-26 15:49:39 +0430

    changed lspsaga icons

--- configLua/lspsaga.lua
+++ configLua/lspsaga.lua
@@ -1,7 +1,7 @@
 local saga = require 'lspsaga'
 saga.init_lsp_saga{
   error_sign = '❌',
-  hint_sign = '⚠️',
-  warn_sign = '☢️',
+  hint_sign = '💡',
+  warn_sign = '⚠️',
   -- border_style = "round"
 }
