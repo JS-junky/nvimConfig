e80f2cca5e4c5dc4c334d8dc3a199d53d2b5ba49
hooman bokhar <hooman@CLI-1-20.avakatan.ir>
2021-12-06 10:42:55 +0330

    added formatter back

--- configLua/typescirpt-language-server.lua
+++ configLua/typescirpt-language-server.lua
@@ -3,7 +3,7 @@ null_ls.config({
     sources = {
         null_ls.builtins.diagnostics.eslint_d, -- eslint or eslint_d
         null_ls.builtins.code_actions.eslint_d, -- eslint or eslint_d
-        null_ls.builtins.formatting.prettier -- prettier, eslint, eslint_d, or prettierd
+        -- null_ls.builtins.formatting.prettier -- prettier, eslint, eslint_d, or prettierd
     },
 })
 
