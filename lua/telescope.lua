d725465a753e31a299843005beb69efafc99e93a
hooman bokhar <hooman@CLI-1-20.avakatan.ir>
2022-07-05 13:59:37 +0430

    deleted a whitesapce

--- lua/telescope.lua
+++ lua/telescope.lua
@@ -1,6 +1,6 @@
 local actions = require('telescope.actions')
 
-require('telescope').setup{
+require('telescope').setup {
   defaults = {
     vimgrep_arguments = {
       'rg',
@@ -11,11 +11,11 @@ require('telescope').setup{
       '--column',
       '--smart-case'
     },
-       mappings = {
-         i = {
-           ["<esc>"] = actions.close,
-           ["<C-c>"] = false,
-         } 
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<C-c>"] = false,
+      }
     },
     prompt_prefix = "> ",
     selection_caret = "> ",
@@ -32,9 +32,9 @@ require('telescope').setup{
         mirror = false,
       },
     },
-    file_sorter =  require'telescope.sorters'.get_fuzzy_file,
-    file_ignore_patterns = {'node_modules'},
-    generic_sorter =  require'telescope.sorters'.get_generic_fuzzy_sorter,
+    file_sorter = require 'telescope.sorters'.get_fuzzy_file,
+    file_ignore_patterns = { 'node_modules' },
+    generic_sorter = require 'telescope.sorters'.get_generic_fuzzy_sorter,
     winblend = 0,
     border = {},
     borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
@@ -42,11 +42,11 @@ require('telescope').setup{
     use_less = true,
     path_display = {},
     set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-    file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
-    grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
-    qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+    file_previewer = require 'telescope.previewers'.vim_buffer_cat.new,
+    grep_previewer = require 'telescope.previewers'.vim_buffer_vimgrep.new,
+    qflist_previewer = require 'telescope.previewers'.vim_buffer_qflist.new,
 
     -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
+    buffer_previewer_maker = require 'telescope.previewers'.buffer_previewer_maker
   }
 }
